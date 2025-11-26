6e7ef230524f34d8285467fdf928431b1421e1e9
developer <patoliya.oj@gmail.com>
2025-11-26 11:54:32 +0530

    complete register and login

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,6 +16,7 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'name' => $this->name,
             'email' => $this->email,
             'created_at' => $this->created_at->format('Y-m-d H:i:s')
         ];
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+// use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisterController::class, 'addAdmin']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [RegisterController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {});
